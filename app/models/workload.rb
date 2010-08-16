7fb86d1ef6bae7a4590224aa44eed07acd7bc7ca
jbbarth <jeanbaptiste.barth@gmail.com>
2010-08-16 08:02:34 +0200

    Moved helpers to a Workload model

---
+++ app/models/workload.rb
@@ -0,0 +1,24 @@
+class Workload
+  def versions
+    @versions ||= Version.all(:conditions => [
+      "project_id IN (?) AND effective_date BETWEEN ? AND ?", 
+       projects, date_from, date_to
+    ])
+  end
+  
+  def projects
+    if @project
+      @project.self_and_descendants
+    else
+      Project.all
+    end
+  end
+  
+  def date_from
+    Date.today - 3000
+  end
+  
+  def date_to
+    Date.today + 150
+  end
+end
