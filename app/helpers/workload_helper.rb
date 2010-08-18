36a7fb8adcc6f588d9adcc2946deb6c5c68b27fd
jbbarth <jeanbaptiste.barth@gmail.com>
2010-08-18 09:20:16 +0200

    Moved css classes calculation in the helper

--- app/helpers/workload_helper.rb
+++ app/helpers/workload_helper.rb
@@ -1,2 +1,8 @@
 module WorkloadHelper
+  def workload_css_classes_for(version,day)
+    c = []
+    c << "selected" if day.cweek == version.effective_date.cweek
+    c << "today" if day.cweek == Date.today.cweek
+    c.join(" ")
+  end
 end
