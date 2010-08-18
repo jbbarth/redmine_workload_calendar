99c985ebe86c820ae9b86aa8eda58cf669c48c2a
jbbarth <jeanbaptiste.barth@gmail.com>
2010-08-18 22:32:47 +0200

    Added ability to specify the project where to list versions

--- lib/redmine_workload/macros.rb
+++ lib/redmine_workload/macros.rb
@@ -1,12 +1,20 @@
 Redmine::WikiFormatting::Macros.register do
-  desc "Display a workload calendar."
+  desc "Display a workload calendar.\n\n" +
+       "With no argument, only current project and descendants versions are listed.\n" +
+       "On cross-project pages (such as welcome page), all projects versions are listed.\n" +
+       "You can provide an ID or project identifier to list only versions of this project and descendants.\n\n" +
+       "Example:\n" +
+       "  !{{workload}} -- display versions of current project (or all if you're not under a project)\n" +
+       "  !{{workload(myapplication)}} -- display versions of project 'myapplication'\n" +
+       "  !{{workload(2)}} -- display versions of project 2"
 
   macro :workload do |obj, args|
     args, options = extract_macro_options(args, :parent)
-    #return "Error: you should provide an ID" if args.size == 0
+    project = Project.find(args.first) rescue nil
     #TODO: remove this ugly thing
     ActionView::Base.send(:include, WorkloadHelper)
     ActionView::Base.send(:include, ProjectsHelper)
-    render :partial => 'projects/workload'
+    render :partial => 'projects/workload',
+           :locals => {:project => project}
   end
 end
