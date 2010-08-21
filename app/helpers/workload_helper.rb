cf46daae329a15e8fb7e06346ad90f03ce4df2cb
jbbarth <jeanbaptiste.barth@gmail.com>
2010-08-22 01:18:53 +0200

    Display version as links in workload calendar

--- app/helpers/workload_helper.rb
+++ app/helpers/workload_helper.rb
@@ -5,4 +5,9 @@ module WorkloadHelper
     c << "today" if day.cweek == Date.today.cweek
     c.join(" ")
   end
+  
+  def link_to_workload_version(version)
+    link_to_if version.visible?, truncate("#{version.project} - #{version}", :length => 30),
+               { :controller => 'versions', :action => 'show', :id => version }
+  end
 end
