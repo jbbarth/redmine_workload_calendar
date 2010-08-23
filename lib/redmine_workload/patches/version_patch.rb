09083ab5651e0ae31b72b250caabb0f90efab393
jbbarth <jeanbaptiste.barth@gmail.com>
2010-08-23 11:46:51 +0200

    Fixed: wrong ordering by load_start when changing year (now load_start/end return dates objects, not just week numbers)

--- lib/redmine_workload/patches/version_patch.rb
+++ lib/redmine_workload/patches/version_patch.rb
@@ -4,14 +4,14 @@ class Version
   belongs_to :version_load
   
   def load_start
-    @load_start = self.effective_date.cweek - self.version_load.weeks_before
+    @load_start = self.effective_date - self.version_load.weeks_before * 7
   end
   
   def load_end
-    @load_end = self.effective_date.cweek + self.version_load.weeks_after
+    @load_end = self.effective_date + self.version_load.weeks_after * 7
   end
   
   def load_weeks
-    load_start..load_end
+    load_start.cweek..load_end.cweek
   end
 end
