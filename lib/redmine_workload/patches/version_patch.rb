014347d94418cbc26aa6c1b9081e352321a9d44c
jbbarth <jeanbaptiste.barth@gmail.com>
2010-08-23 18:34:54 +0200

    Optimized date calculations in Version patch (Date is extremely slow, so we cache method results in instance variables)

--- lib/redmine_workload/patches/version_patch.rb
+++ lib/redmine_workload/patches/version_patch.rb
@@ -4,18 +4,18 @@ class Version
   belongs_to :version_load
   
   def load_start
-    @load_start = self.effective_date - self.version_load.weeks_before * 7
+    @load_start ||= self.effective_date - self.version_load.weeks_before * 7
   end
   
   def load_end
-    @load_end = self.effective_date + self.version_load.weeks_after * 7
+    @load_end ||= self.effective_date + self.version_load.weeks_after * 7
   end
   
   def load_period
-    load_start..load_end
+    @load_period ||= load_start..load_end
   end
   
   def load_weeks
-    load_period.map(&:cweek).uniq
+    @load_weeks ||= load_period.map(&:cweek).uniq
   end
 end
