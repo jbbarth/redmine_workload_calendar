3fc7c6c77d873e91e55845551438d81707173849
jbbarth <jeanbaptiste.barth@gmail.com>
2010-12-06 19:10:46 +0100

    Fixed nil method errors. TODO: cleanup this!

--- lib/redmine_workload/patches/version_patch.rb
+++ lib/redmine_workload/patches/version_patch.rb
@@ -4,11 +4,11 @@ class Version
   belongs_to :version_load
   
   def load_start
-    @load_start ||= self.effective_date - self.version_load.weeks_before * 7
+    @load_start ||= self.effective_date - (self.version_load.present? ? self.version_load.weeks_before * 7 : 0)
   end
   
   def load_end
-    @load_end ||= self.effective_date + self.version_load.weeks_after * 7
+    @load_end ||= self.effective_date + (self.version_load.present? ? self.version_load.weeks_after * 7 : 0)
   end
   
   def load_period
