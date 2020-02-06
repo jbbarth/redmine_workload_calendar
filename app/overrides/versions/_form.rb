2c88d4e69af084f2ddee2502c426aa5bdd058003
Vincent Robert <vincent.robert@nanego.com>
2020-02-06 14:41:09 +0100

    Fix override and use more robust selector

--- app/overrides/versions/_form.rb
+++ app/overrides/versions/_form.rb
@@ -1,4 +1,4 @@
-Deface::Override.new :virtual_path  => "versions/_form",
+Deface::Override.new :virtual_path       => "versions/_form",
                      :name               => "add-load-to-versions",
-                     :insert_before      => "erb[silent]:contains('@version.custom_field_values')",
+                     :insert_bottom      => "div.tabular",
                      :text               =>  "<p><%= f.select :version_load_id, VersionLoad.all.map {|v| [v.name, v.id]} %></p>"
